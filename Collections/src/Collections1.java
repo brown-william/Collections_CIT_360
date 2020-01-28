0371137891d716edc98a4ab34dd94610918a2a57
Brycen Newell <Brycen Newell@BrycensDesktop>
2020-01-28 17:25:50 -0600

    Addition of Runnable public class Task

--- Collections/src/Collections1.java
+++ Collections/src/Collections1.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class Collections1 {
 
@@ -16,3 +17,34 @@ public class Collections1 {
 	}
 
 }
+
+
+
+public class Task implements Runnable {
+	private String name;
+	public Task(String name) {
+		super();
+		this.name = name;		
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub		
+	}
+	
+}
+public class runnables {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+	
+	
+	Thread t1 = new Thread(new Task("This is the Task 1"));
+	t1.start();
+	Thread t2 = new Thread(new Task("This is the Task 2"));
+	t2.start();
+
+}}
+
