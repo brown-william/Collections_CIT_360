ffa98c69af61033cd17ed67bf61840cb0adad08e
Christopher Newell <cbnewell@byui.edu>
2020-02-13 16:10:35 -0600

    Working on JSON

---
+++ Collections/src/JSONTest.java
@@ -0,0 +1,51 @@
+package com.mkyong;
+
+import com.google.gson.Gson;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GsonExample1 {
+
+    public static void main(String[] args) {
+
+        Gson gson = new Gson();
+
+        Staff staff = createStaffObject();
+
+        // Java objects to String
+        // String json = gson.toJson(staff);
+
+        // Java objects to File
+        try (FileWriter writer = new FileWriter("C:\\projects\\staff.json")) {
+            gson.toJson(staff, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static Staff createStaffObject() {
+
+        Staff staff = new Staff();
+
+        staff.setName("mkyong");
+        staff.setAge(35);
+        staff.setPosition(new String[]{"Founder", "CTO", "Writer"});
+        Map<String, BigDecimal> salary = new HashMap() {{
+            put("2010", new BigDecimal(10000));
+            put("2012", new BigDecimal(12000));
+            put("2018", new BigDecimal(14000));
+        }};
+        staff.setSalary(salary);
+        staff.setSkills(Arrays.asList("java", "python", "node", "kotlin"));
+
+        return staff;
+
+    }
+
+}
